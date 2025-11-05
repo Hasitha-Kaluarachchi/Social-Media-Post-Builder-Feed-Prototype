9daf3ecd27b7d4955ae67d1d114fdf97432a2e05
Nadeeja-git <dilumnadeeja3@gmail.com>
2025-11-05 16:22:15 +0530

    Update PostWall.jsx

--- src/components/PostWall.jsx
+++ src/components/PostWall.jsx
@@ -4,11 +4,6 @@ import SocialPost from "./SocialPost";
 const PostWall = ({ posts, onGoBack }) => {
   return (
     <div className="post-wall">
-      <h2>My Wall ✍️</h2>
-      <button className="back-btn" onClick={onGoBack}>
-        ⬅️ Create New Post
-      </button>
-
       {posts.length > 0 ? (
         posts.map((post) => <SocialPost key={post.id} post={post} />)
       ) : (
